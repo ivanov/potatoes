20bed948c6d39a7d535f71e9af92587f2bc52e67
Stephanie Stattel <sstattel@bloomberg.net>
2018-08-24 10:55:55 -0400

    fixing cellmenu comments

--- cellmenu/src/index.ts
+++ cellmenu/src/index.ts
@@ -15,17 +15,8 @@ import '../style/index.css';
 
 
 /**
- * Extension that adds a new keybinding for the existing Launcher 'create' command
+ * Extension that adds a new cell sub menu
  */
- // you can also use Settings >> Advanced Settings >> Keyboard Shortcuts to set
- // and test User Overrides
- // System Defaults is a good reference for selector options
- // and keys syntax as well (Cntrl versus Cntrl)
- // the User Overrides input area will also validate your bindings
-
- // jupyterlab-vim is a good example of a key binding extension that configures 
- // vim notebook cell bindings for JupyterLab
- // https://github.com/jwkvam/jupyterlab-vim
 const extension: JupyterLabPlugin<void> = {
   id: 'jlab_cell_menu',
   autoStart: true,
